227205b23c7ac05354dbb0f84e39737a576bccba
Manuel Kaufmann <humitos@gmail.com>
2016-09-05 20:55:41 -0500

    Mejora en el script de instalacion de dependencias

--- dev/install_dependencies.sh
+++ dev/install_dependencies.sh
@@ -12,10 +12,6 @@
 # es indispensable, pero esos pasos deberían hacerse a mano de lo
 # contrario.
 
-# Si habilito esta opción, luego de el mkvirtualenv (cuando activa el
-# entorno) se para el script
-# set -e
-
 export PATCH_DIR=`pwd`
 
 # Incluimos el script de virtualenvwrapper (requiere
@@ -24,6 +20,8 @@ export PATCH_DIR=`pwd`
 
 echo "Creando el entorno Python"
 mkvirtualenv python-tutorial
+
+set -e
 
 echo "Instalando pdfrw"
 pip install pdfrw
@@ -35,14 +33,20 @@ patch -p0 < $PATCH_DIR/pdfrw.diff
 
 echo "Descargando el repositorio de rst2pdf"
 cd $PATCH_DIR
-svn checkout -q http://rst2pdf.googlecode.com/svn/trunk/ rst2pdf
+git clone https://github.com/rst2pdf/rst2pdf
 cd rst2pdf
 python setup.py develop
 patch -p0 < $PATCH_DIR/rst2pdf.diff
 
 echo "Instalando Sphinx"
-pip install sphinx # 1.1.3
+pip install sphinx==1.1.3
 pip install sphinx-bootstrap-theme
+
+echo "Instalando Fabric"
+pip install fabric
+
+echo "instalando BeautifulSoup 4"
+pip install bs4
 
 echo "Instalando pdftk"
 sudo apt-get install pdftk
